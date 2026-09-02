eeba2530fba8119bac3d190aa60e334b6a39adf4
cowkite202 <25-588@wooshin.sen.hs.kr>
2026-09-02 15:29:33 +0900

    Update app (1).js

--- app (1).js
+++ app (1).js
@@ -3,7 +3,7 @@
 ===================================================== */
 
 const GOOGLE_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbysXqcNrTxC1_h6vcm6jVCLkMK6Mm41Pu8lsU-LgDZjq6VALYepiZwHQ8LmY-aJRuFOsw/exec";
+  "https://script.google.com/macros/s/AKfycbyhzMPqDxjDzufeQK8bGx1w_pHgXmojpt_kfgJ9Ye36dNA68SQ6s8GO5B7LsePFiY_m/exec";
 
 
 /* =====================================================
